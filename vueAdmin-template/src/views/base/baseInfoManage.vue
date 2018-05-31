3b4f84dc2dc0f95fd893351fe7134af6126be535
loong <a159pfl@gmail.com>
2018-05-31 09:33:53 +0800

    修改页面

--- vueAdmin-template/src/views/base/baseInfoManage.vue
+++ vueAdmin-template/src/views/base/baseInfoManage.vue
@@ -2,7 +2,7 @@
 <div class="app-container calendar-list-container">
   <!-- 搜索栏 -->
   <div class="filter-container">
-    <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="车次的名称" v-model="listQuery.name">         
+    <el-input  style="width: 200px;" class="filter-item" placeholder="车次的名称" v-model="listQuery.name">         
     </el-input>
     <el-input  style="width: 200px;" class="filter-item" placeholder="开始时间" v-model="listQuery.startTime">         
     </el-input>
@@ -13,106 +13,68 @@
     <el-button class="filter-item" style="primary" icon="el-icon-upload el-icon--right" @click="handleUploade">导入</el-button>
   </div>
   <!-- 搜索栏End -->
+
     <!-- 列车信息数据表格 -->
-		<el-table :data="data" v-loading="listLoding" element-loading-text="給我一點時間" border fit highlight-current-row style="width:100%">
-			<el-table-column type="index" align="center" width="35" >
-				<template slot-scope="scope">
-          <span>
-            {{scope.row.id}}
-          </span>
-        </template>
-			</el-table-column>
-			<el-table-column  align="center" label="到达车次" width="200">
-        <template>
-          <span>
-            {{scope.row.arriveName}}
-          </span>
-        </template>       
-      </el-table-column>
-            <el-table-column  align="center" label="到达时间" width="200">
-              <template>               
-                  <span>{{scope.row.arriveDate |parseTime('{y}--{m}-{d} {h}:{i}')}} </span>               
-              </template>
-            </el-table-column>
-            <el-table-column  align="center" label="轨道" width="200">
-              <template>                
-                <span>{{scope.row.trackNum}}</span>              
-              </template>
-            </el-table-column>
-            <el-table-column  align="center" label="出发时间" width="200">
-              <template>                
-                <span>{{scope.row.outSetDate |parseTime('{y}--{m}-{d} {h}:{i}')}}</span>              
-              </template>
-            </el-table-column>
-            <el-table-column  align="center" label="状态" width="200">
-              <template>                
-                <span>{{scope.row.status}}</span>              
-              </template>
-            </el-table-column>
-			<!-- 表格操作工具栏 -->
-			<el-table-column label="操作" align="center" width="150">
-				<template solt-scope="scope">
-					<!-- 行内编辑 -->
-					<el-button :type="!scope.row.isedit ? 'primary' : 'success'" size="small" @click="handleRowEdit(scope.$index, scope.row)">{{!scope.row.isedit ? '编辑' : '确定'}}</el-button>	
-					<el-button type="info" v-show="scope.row.isedit" size="small" @click="cancelRowEdit(scope.$index, scope.row)">取消</el-button>	
-
-					<!-- 删除提示tooltip -->
-					<el-popover ref="popoverDelete" placement="top"  v-model="scope.row.visible">
-						<p>确定要删除这条数据吗?</p>
-						<div style="text-align: center;">
-							<el-button size="mini" type="success" @click="scope.row.visible = false">取消</el-button>
-							<el-button size="mini" type="danger" @click="handleRowDel(scope.$index, scope.row)">确定</el-button>
-						</div>
-						<el-button type="danger" v-show="!scope.row.isedit" size="small" slot="reference" @click="scope.row.visible = true">删除</el-button>
-					</el-popover>
-				</template>
-			</el-table-column>
-			<!-- 表格操作工具栏 End-->
-		</el-table>
+	<el-table :data="list"  element-loading-text="給我一點時間" border fit highlight-current-row style="width:100%">
+		<el-table-column  align="center" label="到达车次" width="200">
+      <template slot-scope="scope">
+        <span>
+          {{scope.row.arriveName}}
+        </span>
+      </template>       
+    </el-table-column>
+          <el-table-column  align="center" label="到达时间" >
+            <template slot-scope="scope">               
+                <span>{{scope.row.arriveDate |parseTime('{y}-{m}-{d} {h}:{i}') }} </span>               
+            </template>
+          </el-table-column>
+          <el-table-column  align="center" label="轨道" >
+            <template slot-scope="scope">                
+              <span>{{scope.row.trackNum}}</span>              
+            </template>
+          </el-table-column>
+          <el-table-column  align="center" label="出发时间" >
+            <template slot-scope="scope">                
+              <span>{{scope.row.outSetDate |parseTime('{y}-{m}-{d} {h}:{i}')}}</span>              
+            </template>
+          </el-table-column>
+          <el-table-column  align="center" label="状态" >
+            <template slot-scope="scope">                
+              <span>{{scope.row.status}}</span>              
+            </template>
+      </el-table-column>			         
+	</el-table>
 		<!-- 列车信息表格 End-->
-
-    <!-- 分页栏 -->
-    <div class="pagination-container">
-      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.pageIndex" :page-sizes="[10,20,30, 50]" :page-size="listQuery.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total">
-      </el-pagination>
-    </div>
-    <!-- 分页栏 End -->
-
-<!-- 弹出对话框层-->
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFornVisible">
-      <el-form rules="rules" ref="dataForm" :model="trainInfo" label-position="left">
-        <el-form-item label="到达车次" prop="arriveName">
-          <el-input type="text" v-model="trainInfo.arriveName" ></el-input>
-        </el-form-item>
-        <el-form-item label="出发车次" prop="outSetName">
-          <el-input type="text" v-model="trainInfo.outSetName" ></el-input>
-        </el-form-item>
-        <el-form-item label="股道" prop="trackNum">
-          <el-input v-model="trainInfo.trackNum" type="munber">
-          </el-input>
-        </el-form-item>
-        <el-form-item label="到达时间" prop="arriveDate">
-          <el-date-picker v-model="trainInfo.arriveDate" type="datetime" placeholder="请选择时间！">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="出发时间" prop="outSetDate">
-          <el-date-picker v-model="trainInfo.outSetDate" type="datetime" placeholder="请选择时间!">
-          </el-date-picker>
-        </el-form-item>
-      </el-form> 
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">{{$t('table.cancel')}}</el-button>
-        <el-button v-if="dialogStatus=='create'" type="primary" @click="createData">{{$t('table.confirm')}}</el-button>
-        <el-button v-else type="primary" @click="updateData">{{$t('table.confirm')}}</el-button>
-      </div>    
-    </el-dialog>
-<!-- 弹出对话框层 End-->
+    <!--对话框-->
+<el-dialog title="基础信息" :visible.sync="dialogFormVisible" label-position="left" label-width="70px" style='width: 400px; margin-left:50px;'>
+  <el-form :model="trainInfo">
+    <el-form-item label="到达车次">
+      <el-input type="text" v-model="trainInfo.arriveName"> </el-input>
+    </el-form-item>
+    <el-form-item lable="到达时间">
+      <el-date-picker type="datetime" v-model="trainInfo.arriveDate"></el-date-picker>
+    </el-form-item>
+    <el-form-item label="轨道">
+      <el-input v-model="trainInfo.trackNum"></el-input>
+    </el-form-item>
+    <el-form-item lable="出发时间">
+      <el-date-picker type="datetime" v-model="trainInfo.outSetDate"></el-date-picker>
+    </el-form-item>
+    <el-form-item label="出发车次">
+      <el-input v-model="trainInfo.outSetName"></el-input>
+    </el-form-item>
+   </el-form>
+</el-dialog>
+<!---->
   </div>
 </template>
 
+
+
 <script>
 import request from "@/utils/request";
-
+import { parseTime } from "@/utils";
+import { fetchList } from "@/api/train";
 export default {
   data() {
     return {
@@ -126,49 +88,80 @@ export default {
         startTime: undefined,
         endTime: undefined
       },
-      trainInfo:{
-        id:undefined,
-        arriveName:'',
-        outSetName:'',
-        arriveDate:new Date(),
-        outSetDate:new Date(),
-        trackNum:1
+      trainInfo: {
+        id: undefined,
+        arriveName: "",
+        outSetName: "",
+        arriveDate: new Date(),
+        outSetDate: new Date(),
+        trackNum: 1
       },
       dialogFormVisible: false,
-      dialogStatus: '',
+      dialogStatus: "",
       textMap: {
-        update: 'Edit',
-        create: 'Create'
+        update: "Edit",
+        create: "Create"
       },
       dialogPvVisible: false,
       rules: {
-        arriveName: [{ required: true, message: '到达车次必填', trigger: 'change' }],
-        arriveDate: [{ type: 'date', required: true, message: '到达时间必填', trigger: 'change' }],
-        outSetDate: [{ type: 'date', required: true, message: '出发时间必填', trigger: 'change' }],
-        outSetName: [{ required: true, message: '出发车次必填', trigger: 'blur' }],
-        trackNum: [{ required: true, message: '股道序号必填', trigger: 'blur' },
-        {type:'number',min:1,max:13,message:'轨道的序号在1-13内的范围',trigger:'blur'}
+        arriveName: [
+          { required: true, message: "到达车次必填", trigger: "change" }
+        ],
+        arriveDate: [
+          {
+            type: "date",
+            required: true,
+            message: "到达时间必填",
+            trigger: "change"
+          }
+        ],
+        outSetDate: [
+          {
+            type: "date",
+            required: true,
+            message: "出发时间必填",
+            trigger: "change"
+          }
+        ],
+        outSetName: [
+          { required: true, message: "出发车次必填", trigger: "blur" }
+        ],
+        trackNum: [
+          { required: true, message: "股道序号必填", trigger: "blur" },
+          {
+            type: "number",
+            min: 1,
+            max: 13,
+            message: "轨道的序号在1-13内的范围",
+            trigger: "blur"
+          }
         ]
-      },
+      }
     };
   },
-  mounted() {
-    request({
-      url: "/getBaseInfo",
-      methods: "post",
-      data: { a: 1, b: 2 }
-    }).then(
-      response => {
-        console.log("ajax response!!!!!");
-      },
-      response => {
-        console.log("ajax can not work!!!");
-      }
-    );
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      fetchList(this.listQuery).then(response => {
+        this.list = response.list;
+        this.total = response.count;
+      });
+    },
+    handleFilter() {
+      this.listQuery.pageIndex = 1;
+      this.getList();
+    },
+    handleCreate() {
+      //新添加
+      this.dialogStatus = "create";
+      this.dialogFormVisible = true;
+    },
+    handleUploade() {}
   }
 };
 </script>
 
 <style lang="css" scoped>
-
 </style>
